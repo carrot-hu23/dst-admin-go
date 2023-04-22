dbe29aa2580eb0813743db8e5cff3a916bf98e52
jinbohu <1762858544@qq.com>
2023-04-22 14:31:42 +0800

    修改mod返回值

--- api/modApi.go
+++ api/modApi.go
@@ -4,6 +4,7 @@ import (
 	"dst-admin-go/entity"
 	"dst-admin-go/mod"
 	"dst-admin-go/vo"
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
@@ -33,10 +34,27 @@ func SearchModList(ctx *gin.Context) {
 func GetModInfo(ctx *gin.Context) {
 
 	moId := ctx.Param("modId")
+	modinfo := mod.GetModInfo(moId)
+
+	var mod_config map[string]interface{}
+	json.Unmarshal([]byte(modinfo.ModConfig), &mod_config)
+	mod := map[string]interface{}{
+		"auth":          modinfo.Auth,
+		"consumer_id":   modinfo.ConsumerAppid,
+		"creator_appid": modinfo.CreatorAppid,
+		"description":   modinfo.Description,
+		"file_url":      modinfo.FileUrl,
+		"modid":         modinfo.Modid,
+		"img":           modinfo.Img,
+		"last_time":     modinfo.LastTime,
+		"name":          modinfo.Name,
+		"v":             modinfo.V,
+		"mod_config":    mod_config,
+	}
 	ctx.JSON(http.StatusOK, vo.Response{
 		Code: 200,
 		Msg:  "success",
-		Data: mod.GetModInfo(moId),
+		Data: mod,
 	})
 }
 
@@ -47,10 +65,30 @@ func GetMyModList(ctx *gin.Context) {
 
 	db.Find(&modInfos)
 
+	var modDataList []map[string]interface{}
+	for _, modinfo := range modInfos {
+		var mod_config map[string]interface{}
+		json.Unmarshal([]byte(modinfo.ModConfig), &mod_config)
+		mod := map[string]interface{}{
+			"auth":          modinfo.Auth,
+			"consumer_id":   modinfo.ConsumerAppid,
+			"creator_appid": modinfo.CreatorAppid,
+			"description":   modinfo.Description,
+			"file_url":      modinfo.FileUrl,
+			"modid":         modinfo.Modid,
+			"img":           modinfo.Img,
+			"last_time":     modinfo.LastTime,
+			"name":          modinfo.Name,
+			"v":             modinfo.V,
+			"mod_config":    mod_config,
+		}
+		modDataList = append(modDataList, mod)
+	}
+
 	ctx.JSON(http.StatusOK, vo.Response{
 		Code: 200,
 		Msg:  "success",
-		Data: modInfos,
+		Data: modDataList,
 	})
 
 }
