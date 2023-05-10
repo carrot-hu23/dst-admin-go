07de852350572ca01968d8255300e42d4c15ce3c
hu, jinbo <hujb@paratera.com>
2023-05-10 17:45:05 +0800

    添加 BackupSchedule

--- task/backupTask.go
+++ task/backupTask.go
@@ -1,1 +1,33 @@
 package task
+
+import (
+	"dst-admin-go/utils/zip"
+	"fmt"
+	"time"
+)
+
+func BackupSchedule(sourceDir, targetZip string, hour, minute int) {
+	for {
+		now := time.Now()
+		year, month, day := now.Date()
+
+		targetTime := time.Date(year, month, day, hour, minute, 0, 0, now.Location())
+
+		if targetTime.Before(now) {
+			targetTime = targetTime.Add(24 * time.Hour) // 如果目标时间已过，则设置为下一天的目标时间
+		}
+
+		timeToWait := targetTime.Sub(now)
+		fmt.Printf("下一次压缩将在 %v 后进行\n", timeToWait)
+
+		timer := time.NewTimer(timeToWait)
+		<-timer.C
+
+		err := zip.Zip(sourceDir, targetZip)
+		if err != nil {
+			fmt.Println("压缩目录时发生错误:", err)
+		} else {
+			fmt.Println("目录已成功压缩！")
+		}
+	}
+}
