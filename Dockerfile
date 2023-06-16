e02f06522495f80876e484669faf71c50b2b38ec
hu, jinbo <hujb@paratera.com>
2023-06-16 17:14:14 +0800

    添加dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,40 @@
-# 基础镜像
-FROM debian:stretch-slim
+FROM debian:stable-slim
 
-# 设置维护者信息
-LABEL maintainer="your-name <your-email>"
+RUN sed -i 's|deb.debian.org|mirrors.aliyun.com|g' /etc/apt/sources.list && \
+    sed -i 's|security.debian.org|mirrors.aliyun.com/debian-security|g' /etc/apt/sources.list && \
+    dpkg --add-architecture i386
 
-# # 更新 apt 软件包索引并安装基础依赖
-# RUN apt-get update && \
-#     apt-get install -y ca-certificates && \
-#     rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    lib32stdc++6 \
+    libgcc1 \
+    libcurl4-gnutls-dev:i386 \
+    wget \
+    screen \
+    procps && \
+    rm -rf /var/lib/apt/lists/*
 
-# 安装需要的依赖
-RUN dpkg --add-architecture i386 \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends --no-install-suggests  \
-        libstdc++6:i386 \
-        libgcc1:i386 \
-        lib32gcc1 \
-        lib32stdc++6 \
-        libcurl4-gnutls-dev:i386 \
-        wget \
-        ca-certificates \
-        openjdk-8-jre \
-        screen \
-        sudo \
-    && apt-get clean
 
 # 设置工作目录
 WORKDIR /app
 
 # 拷贝程序二进制文件
 COPY dst-admin-go /app/dst-admin-go
+RUN chmod 755 /app/dst-admin-go
 
-# 拷贝配置文件和静态文件
+COPY run.sh /app/run.sh
+RUN chmod 755 /app/run.sh
+
 COPY config.yml /app/config.yml
 COPY dst_config /app/dst_config
 COPY dist /app/dist
+COPY static /app/static
 
-# 暴露端口
+# 内嵌源配置信息
+
 EXPOSE 8082/tcp
 EXPOSE 10888/udp
 EXPOSE 10998/udp
 EXPOSE 10999/udp
 
 # 运行命令
-ENTRYPOINT ["/app/dst-admin-go"]
+ENTRYPOINT ["./run.sh"]
---
+++ run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# 获取传入的参数
+steam_cmd_path='/app/steamcmd'
+steam_dst_server='/app/dst'
+
+# 判断 steam_cmd_path 是否存在，不存在则创建
+if [ ! -d "$steam_cmd_path" ]; then
+  mkdir -p "$steam_cmd_path"
+fi
+
+# 进入 steam_cmd_path 目录
+cd "$steam_cmd_path"
+
+# 如果 $steam_dst_server 目录不存在，则下载并解压 SteamCMD 并安装游戏服务器
+retry=1
+while [ ! -d "${steam_cmd_path}" ] || [ ! -e "${steam_cmd_path}/steamcmd.sh" ]; do
+  if [ $retry -gt 3 ]; then
+    echo "Download steamcmd failed after three times"
+    exit -2
+  fi
+  echo "Not found steamcmd, start to installing steamcmd, try: ${retry}"
+  wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz -P $steam_cmd_path
+  tar -zxvf $steam_cmd_path/steamcmd_linux.tar.gz -C $steam_cmd_path
+  sleep 3
+  ((retry++))
+done
+
+# 如果 $steam_dst_server 目录不存在，则下载并解压 SteamCMD 并安装游戏服务器
+retry=1
+while [ ! -e "${steam_dst_server}/bin/dontstarve_dedicated_server_nullrenderer" ]; do
+  if [ $retry -gt 3 ]; then
+    echo "Download Dont Starve Together Sever failed after three times"
+    exit -2
+  fi
+  echo "Not found Dont Starve Together Sever, start to installing, try: ${retry}"
+  bash $steam_cmd_path/steamcmd.sh +force_install_dir $steam_dst_server +login anonymous +app_update 343050 validate +quit
+  cp $steam_cmd_path/linux32/libstdc++.so.6 $steam_dst_server/bin/lib32/
+  mkdir -p $USER_DIR/.klei/DoNotStarveTogether/MyDediServer
+  echo "steamcmd=$steam_cmd_path" >> /app/dst_config
+  echo "force_install_dir=$steam_dst_server" >> /app/dst_config
+  echo "cluster=MyDediServer" >> /app/dst_config
+  echo "backup=$USER_DIR/.klei/DoNotStarveTogether/MyDediServer" >> /app/dst_config
+  echo "mod_download_path=$USER_DIR/.klei/DoNotStarveTogether/MyDediServer" >> /app/dst_config
+  echo "username=admin" >> /app/password.txt
+  echo "password=123456" >> /app/password.txt
+  echo "displayName=admin" >> /app/password.txt
+  echo "photoURL=xxx" >> /app/password.txt
+  sleep 3
+  ((retry++))
+done
+
+
+# 运行其他命令，这里只是做示例
+echo "SteamCMD installed at $steam_cmd_path"
+echo "SteamDST server installed at $steam_dst_server"
+
+
+cd /app
+./dst-admin-go
