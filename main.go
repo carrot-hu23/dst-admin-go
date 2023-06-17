64b0bdca63f7ea17a36bb8ac449d67726b03794c
jinbohu <1762858544@qq.com>
2023-06-17 14:45:58 +0800

    添加dockerfile README.md

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"dst-admin-go/config/global"
 	"dst-admin-go/initConfig"
 	"dst-admin-go/router"
+	"embed"
 	"fmt"
 )
 
 func init() {
 	initConfig.Init()
 }
+
+// 嵌入为一个文件系统 新的文件系统FS
+//
+//go:embed dist
+//go:embed static
+var f embed.FS
 
 func main() {
 
