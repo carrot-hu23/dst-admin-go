7a6b263b9469ec7728de42713f8f5042c877c188
jinbohu <1762858544@qq.com>
2023-08-20 13:32:55 +0800

    修复读取日志文件

--- utils/fileUtils/fileUtls.go
+++ utils/fileUtils/fileUtls.go
@@ -144,46 +144,29 @@ func WriterLnFile(filename string, lines []string) error {
 	return w.Flush()
 }
 
-func ReverseRead(name string, lineNum uint) ([]string, error) {
-	//打开文件
-	file, err := os.Open(name)
+func ReverseRead(filename string, n uint) ([]string, error) {
+	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
-	//获取文件大小
-	fs, err := file.Stat()
-	if err != nil {
+
+	lines := make([]string, 0, n)
+	scanner := bufio.NewScanner(file)
+
+	// Read lines in reverse order
+	for scanner.Scan() {
+		lines = append([]string{scanner.Text()}, lines...)
+		if len(lines) > int(n) {
+			lines = lines[:n]
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
-	fileSize := fs.Size()
 
-	var offset int64 = -1   //偏移量，初始化为-1，若为0则会读到EOF
-	char := make([]byte, 1) //用于读取单个字节
-	lineStr := ""           //存放一行的数据
-	buff := make([]string, 0, 100)
-	for (-offset) <= fileSize {
-		//通过Seek函数从末尾移动游标然后每次读取一个字节
-		file.Seek(offset, io.SeekEnd)
-		_, err := file.Read(char)
-		if err != nil {
-			return buff, err
-		}
-		if char[0] == '\n' {
-			offset--  //windows跳过'\r'
-			lineNum-- //到此读取完一行
-			buff = append(buff, lineStr)
-			lineStr = ""
-			if lineNum == 0 {
-				return buff, nil
-			}
-		} else {
-			lineStr = string(char) + lineStr
-		}
-		offset--
-	}
-	buff = append(buff, lineStr)
-	return buff, nil
+	return lines, nil
 }
 
 func DeleteFile(path string) error {
