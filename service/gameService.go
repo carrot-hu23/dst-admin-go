fa366af2537c797ddcef469c38f5528fb8a59593
hu, jinbo <hujb@paratera.com>
2023-12-28 17:12:05 +0800

    修复 luajit 启动命令

--- service/gameService.go
+++ service/gameService.go
@@ -170,7 +170,7 @@ func (g *GameService) LaunchLevel(clusterName, level string, bin, beta int) {
 	if bin == 64 {
 		startCmd = "cd " + dstInstallDir + "/bin64 ; screen -d -m -S \"" + screenKey.Key(clusterName, level) + "\"  ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster " + clusterName + " -shard " + level
 	} else if bin == 100 {
-		startCmd = "cd " + dstInstallDir + "/bin ; screen -d -m -S \"" + screenKey.Key(clusterName, level) + "\"  ./dontstarve_dedicated_server_nullrenderer_x64_luajit" + clusterName + " -shard " + level
+		startCmd = "cd " + dstInstallDir + "/bin ; screen -d -m -S \"" + screenKey.Key(clusterName, level) + "\"  ./dontstarve_dedicated_server_nullrenderer_x64_luajit -console -cluster" + clusterName + " -shard " + level
 	} else {
 		startCmd = "cd " + dstInstallDir + "/bin ; screen -d -m -S \"" + screenKey.Key(clusterName, level) + "\"  ./dontstarve_dedicated_server_nullrenderer -console -cluster " + clusterName + " -shard " + level
 	}
