b55464632e78d2fc7be34dd0d0adac368007abfa
hu, jinbo <hujb@paratera.com>
2024-01-08 16:38:32 +0800

    获取玩家列表增加缓存

--- service/playerService.go
+++ service/playerService.go
@@ -10,60 +10,174 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+type entry struct {
+	res        result
+	ready      chan struct{} // closed when res is ready
+	expiration time.Time     // expiration time
+}
+
+type Func func(key string) (interface{}, error)
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]*entry)}
+}
+
+type Memo struct {
+	f     Func
+	mu    sync.Mutex // guards cache
+	cache map[string]*entry
+}
+
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	e := memo.cache[key]
+	if e == nil || e.expiration.Before(time.Now()) {
+		// This is the first request for this key or it has expired.
+		// This goroutine becomes responsible for computing
+		// the value and broadcasting the ready condition.
+		e = &entry{
+			ready:      make(chan struct{}),
+			expiration: time.Now().Add(5 * time.Second), // set expiration time to 1 minute from now
+		}
+		memo.cache[key] = e
+		memo.mu.Unlock()
+
+		e.res.value, e.res.err = memo.f(key)
+		close(e.ready) // broadcast ready condition
+	} else {
+		// This is a repeat request for this key and it has not expired.
+		memo.mu.Unlock()
+
+		<-e.ready // wait for ready condition
+	}
+	return e.res.value, e.res.err
+}
+
+var memo *Memo
+
+func init() {
+	memo = New(func(key string) (interface{}, error) {
+		split := strings.Split(key, ":")
+		log.Println("-----------", key)
+		if len(split) != 2 {
+			return []vo.PlayerVO{}, nil
+		}
+		clusterName := split[0]
+		levelName := split[1]
+		if !gameServe.GetLevelStatus(clusterName, levelName) {
+			return []vo.PlayerVO{}, nil
+		}
+
+		id := strconv.FormatInt(time.Now().Unix(), 10)
+
+		command := "for i, v in ipairs(TheNet:GetClientTable()) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
+		// command := "for i, v in ipairs(AllPlayers) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
+
+		playerCMD := "screen -S \"" + screenKey.Key(clusterName, levelName) + "\" -p 0 -X stuff \"" + command + "\\n\""
+
+		shellUtils.Shell(playerCMD)
+
+		time.Sleep(time.Duration(1) * time.Second)
+
+		// TODO 如果只启动了洞穴，应该去读取洞穴的日志
+
+		// 读取日志
+		dstLogs := dstUtils.ReadLevelLog(clusterName, levelName, 100)
+		playerVOList := make([]vo.PlayerVO, 0)
+
+		for _, line := range dstLogs {
+			if strings.Contains(line, id) && strings.Contains(line, "KU") && !strings.Contains(line, "Host") {
+				str := strings.Split(line, " ")
+				log.Println("players:", str)
+				playerVO := vo.PlayerVO{Key: str[2], Day: str[3], KuId: str[4], Name: str[5], Role: str[6]}
+				playerVOList = append(playerVOList, playerVO)
+			}
+		}
+
+		// 创建一个map，用于存储不重复的KuId和对应的PlayerVO对象
+		uniquePlayers := make(map[string]vo.PlayerVO)
+
+		// 遍历players切片
+		for _, player := range playerVOList {
+			// 将PlayerVO对象添加到map中，以KuId作为键
+			uniquePlayers[player.KuId] = player
+		}
+
+		// 将不重复的PlayerVO对象从map中提取到新的切片中
+		filteredPlayers := make([]vo.PlayerVO, 0, len(uniquePlayers))
+		for _, player := range uniquePlayers {
+			filteredPlayers = append(filteredPlayers, player)
+		}
+		return filteredPlayers, nil
+	})
+}
 
 type PlayerService struct {
 }
 
 func (p *PlayerService) GetPlayerList(clusterName string, levelName string) []vo.PlayerVO {
-
-	if !gameServe.GetLevelStatus(clusterName, levelName) {
-		return make([]vo.PlayerVO, 0)
+	value, err := memo.Get(clusterName + ":" + levelName)
+	if err != nil {
+		log.Println("[warring]", err)
+		return []vo.PlayerVO{}
 	}
+	return value.([]vo.PlayerVO)
 
-	id := strconv.FormatInt(time.Now().Unix(), 10)
-
-	command := "for i, v in ipairs(TheNet:GetClientTable()) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
-	// command := "for i, v in ipairs(AllPlayers) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
-
-	playerCMD := "screen -S \"" + screenKey.Key(clusterName, levelName) + "\" -p 0 -X stuff \"" + command + "\\n\""
-
-	shellUtils.Shell(playerCMD)
-
-	time.Sleep(time.Duration(1) * time.Second)
-
-	// TODO 如果只启动了洞穴，应该去读取洞穴的日志
-
-	// 读取日志
-	dstLogs := dstUtils.ReadLevelLog(clusterName, levelName, 100)
-	playerVOList := make([]vo.PlayerVO, 0)
-
-	for _, line := range dstLogs {
-		if strings.Contains(line, id) && strings.Contains(line, "KU") && !strings.Contains(line, "Host") {
-			str := strings.Split(line, " ")
-			log.Println("players:", str)
-			playerVO := vo.PlayerVO{Key: str[2], Day: str[3], KuId: str[4], Name: str[5], Role: str[6]}
-			playerVOList = append(playerVOList, playerVO)
-		}
-	}
-
-	// 创建一个map，用于存储不重复的KuId和对应的PlayerVO对象
-	uniquePlayers := make(map[string]vo.PlayerVO)
-
-	// 遍历players切片
-	for _, player := range playerVOList {
-		// 将PlayerVO对象添加到map中，以KuId作为键
-		uniquePlayers[player.KuId] = player
-	}
-
-	// 将不重复的PlayerVO对象从map中提取到新的切片中
-	filteredPlayers := make([]vo.PlayerVO, 0, len(uniquePlayers))
-	for _, player := range uniquePlayers {
-		filteredPlayers = append(filteredPlayers, player)
-	}
-
-	return filteredPlayers
+	//if !gameServe.GetLevelStatus(clusterName, levelName) {
+	//	return make([]vo.PlayerVO, 0)
+	//}
+	//
+	//id := strconv.FormatInt(time.Now().Unix(), 10)
+	//
+	//command := "for i, v in ipairs(TheNet:GetClientTable()) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
+	//// command := "for i, v in ipairs(AllPlayers) do  print(string.format(\\\"%s %d %s %s %s %s \\\", " + "'" + id + "'" + ",i-1, string.format('%03d', v.playerage), v.userid, v.name, v.prefab)) end"
+	//
+	//playerCMD := "screen -S \"" + screenKey.Key(clusterName, levelName) + "\" -p 0 -X stuff \"" + command + "\\n\""
+	//
+	//shellUtils.Shell(playerCMD)
+	//
+	//time.Sleep(time.Duration(1) * time.Second)
+	//
+	//// TODO 如果只启动了洞穴，应该去读取洞穴的日志
+	//
+	//// 读取日志
+	//dstLogs := dstUtils.ReadLevelLog(clusterName, levelName, 100)
+	//playerVOList := make([]vo.PlayerVO, 0)
+	//
+	//for _, line := range dstLogs {
+	//	if strings.Contains(line, id) && strings.Contains(line, "KU") && !strings.Contains(line, "Host") {
+	//		str := strings.Split(line, " ")
+	//		log.Println("players:", str)
+	//		playerVO := vo.PlayerVO{Key: str[2], Day: str[3], KuId: str[4], Name: str[5], Role: str[6]}
+	//		playerVOList = append(playerVOList, playerVO)
+	//	}
+	//}
+	//
+	//// 创建一个map，用于存储不重复的KuId和对应的PlayerVO对象
+	//uniquePlayers := make(map[string]vo.PlayerVO)
+	//
+	//// 遍历players切片
+	//for _, player := range playerVOList {
+	//	// 将PlayerVO对象添加到map中，以KuId作为键
+	//	uniquePlayers[player.KuId] = player
+	//}
+	//
+	//// 将不重复的PlayerVO对象从map中提取到新的切片中
+	//filteredPlayers := make([]vo.PlayerVO, 0, len(uniquePlayers))
+	//for _, player := range uniquePlayers {
+	//	filteredPlayers = append(filteredPlayers, player)
+	//}
+	//
+	//return filteredPlayers
 
 }
 
