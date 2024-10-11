68f3f5842fd65b8022bd2fbf6ea4e939d885453a
hu, jinbo <hujb@paratera.com>
2024-10-11 12:09:12 +0800

    调整cluster路由

--- middleware/proxyHandle.go
+++ middleware/proxyHandle.go
@@ -38,7 +38,7 @@ func Proxy(c *gin.Context) {
 	baseURL := urlParts[0]
 
 	// 检查路径是否在白名单中
-	if !strings.Contains(baseURL, "/api") || whiteList[baseURL] {
+	if !strings.Contains(baseURL, "/api") || whiteList[baseURL] || strings.Contains(baseURL, "/api/cluster/detail") {
 		// 如果在白名单中，直接处理请求
 		c.Next()
 		return
