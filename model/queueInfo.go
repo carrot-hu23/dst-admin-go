fd4501a38d31876919e6a5c5fbfd8e8122b4e213
hu, jinbo <hujb@paratera.com>
2024-10-15 11:18:31 +0800

    增加queue

--- model/queueInfo.go
+++ model/queueInfo.go
@@ -2,10 +2,13 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type QueueInfo struct {
 	gorm.Model
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 	Name      string ` json:"name"`
 	QueueCode string `gorm:"uniqueIndex" json:"queueCode"`
--- model/zoneInfo.go
+++ model/zoneInfo.go
@@ -2,10 +2,13 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type ZoneInfo struct {
 	gorm.Model
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 	Name     string `json:"name"`
 	ZoneCode string `gorm:"uniqueIndex" json:"zoneCode"`
