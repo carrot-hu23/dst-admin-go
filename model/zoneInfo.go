13be0665940d0201970befaa40ddf8281f5499f7
hu, jinbo <hujb@paratera.com>
2024-10-16 13:35:20 +0800

    调整架构，数据库改成mysql

--- model/zoneInfo.go
+++ model/zoneInfo.go
@@ -2,11 +2,11 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type ZoneInfo struct {
 	gorm.Model
-	CreatedAt time.Time
-	UpdatedAt time.Time
+
+	Name     string `json:"name"`
+	ZoneCode string `gorm:"uniqueIndex" json:"zoneCode"`
 }
